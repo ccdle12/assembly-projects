a512253879706657fd6de69a8c757c0d86d7f88b
Christopher Coverdale <chris.coverdale24@gmail.com>
2020-12-05 18:13:49 +0000

    add external-files and macros-equ

---
+++ external-files/external_file.asm
@@ -0,0 +1,36 @@
+section .data
+    digit db 0,10 ; store x2 bytes at [digit] - (0, 10). 10 is the new line character
+
+; Use equ to define constants
+STDIN equ 0
+STDOUT equ 1
+
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_EXIT equ 60
+
+; This macro takes 0 arguements and exits the program.
+%macro exit 0
+    mov rax, SYS_EXIT
+    mov rdi, 0
+    syscall
+%endmacro
+
+; This macro takes 1 arguement, the digit that will be printed and prints
+; the ASCII represenation of the digit.
+%macro printDigit 1
+    mov rax, %1
+    call _printRAXDigit
+%endmacro
+
+_printRAXDigit:
+    add rax, 48
+    mov [digit], al ; Move the LSB in rax (the al register) into the memory
+                    ; address of digit.
+
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, digit ; Move the digit to the rsi register.
+    mov rdx, 2
+    syscall
+    ret
---
+++ external-files/macros.asm
@@ -0,0 +1,8 @@
+%include "external_file.asm"
+
+section .text
+    global _start
+
+_start:
+    printDigit 3
+    exit
